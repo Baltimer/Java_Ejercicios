6f126fd7a15109598d5f6ce91ccaddaa2886fcd2
Luis Cifre <Luis Cifre>
2017-02-13 19:09:33 +0100

    Inicializado Kata de Medicine Clash

---
+++ MedicineClash_Kata/src/org/foobarspam/MedicineClash/Prescription/Prescription.java
@@ -0,0 +1,14 @@
+package org.foobarspam.MedicineClash.Prescription;
+import java.util.Date;
+
+public class Prescription {
+    
+    private Date dispenseDate = new Date();
+    private int daysSupply = 30;
+    
+    public Prescription(Date dispenseDate, int daysSupply) {
+        this.dispenseDate = dispenseDate;
+        this.daysSupply = daysSupply;
+    }
+
+}
